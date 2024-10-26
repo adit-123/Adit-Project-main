eee80a1451be7b75098479193ef73ff470dbe80b
Adit Bhavani <aditbhavani2212@gmail.com>
2024-10-26 17:58:26 +0530

    error commit

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin:process.env.CLIENT_URL
-}));
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 
 // Connect to MongoDB
